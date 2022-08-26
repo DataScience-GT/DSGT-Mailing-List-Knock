7219664627bb3b66075aa59e795e4700fe65cb45
JohnRamberger <jramberger3@gatech.edu>
2022-08-25 22:03:54 -0400

    setup app

---
+++ bulkemail.ts
@@ -0,0 +1,8 @@
+// import{ Knock } from "@knocklabs/node";
+// import dotenv from "dotenv";
+// dotenv.config();
+// const knock = new Knock(process.env.KNOCK_API_KEY)
+
+// console.log(knock);
+
+console.log("Hello World")
